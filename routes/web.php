4bf771c2506b743134fd2ab9da81e9814c1c1b4b
徐承恩 <771247770@qq.com>
2022-12-06 20:10:38 +0800

    创建项目

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return response('中华人民共和国');
+    return response('中华人民共和国香港特别行政区');
 })->middleware('lscache:max-age=300;public');
