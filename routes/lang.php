89d801e5e3691ffd0a60ea9e8fc974ba2eed9a50
徐承恩 <771247770@qq.com>
2022-12-06 22:05:19 +0800

    创建项目

--- routes/lang.php
+++ routes/lang.php
@@ -2,4 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->middleware('lscache:max-age=300;public');
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'index']);//->middleware('lscache:max-age=300;public');
