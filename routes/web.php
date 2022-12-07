c530f591b3cc5e03bb6bef1d2afb0725da8826c5
徐承恩 <771247770@qq.com>
2022-12-07 17:49:48 +0800

    创建项目

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Litespeed\LSCache\LSCache;
 
-Route::get('/', [\App\Http\Controllers\HomeController::class, 'index']);//->middleware('lscache:max-age=300;public');
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->middleware('lscache:max-age=300;public');
 
 Route::get('/clear', function () {
     LSCache::purgeAll();
